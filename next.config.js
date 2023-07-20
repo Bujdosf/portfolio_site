7669532e0b76532e4c66fa9c43311cc439ea0ff9
Bujdosf <bujdos.f01@gmail.com>
2023-07-20 14:19:59 +0200

    GithubPages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio_site/' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/portfolio_site' : '',
+  };
